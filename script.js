ea35e00d6c38205df52eb67e8c37f0de8c0eaa83
Ramyaa S <ramyaasuresh01@gmail.com>
2021-07-19 14:50:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,94 @@
+numOps = 0
+function getHistory(){
+	return document.getElementById("history-value").innerText;
+}
+function printHistory(num){
+	document.getElementById("history-value").innerText=num;
+}
+function getOutput(){
+	return document.getElementById("output-value").innerText;
+}
+function printOutput(num){
+	if(num==""){
+		document.getElementById("output-value").innerText=num;
+	}
+	else{
+		document.getElementById("output-value").innerText=getFormattedNumber(num);
+	}	
+}
+function getFormattedNumber(num){
+	if(num=="-"){
+		return "";
+	}
+	var n = Number(num);
+	var value = n.toLocaleString("en");
+	return value;
+}
+function reverseNumberFormat(num){
+	return Number(num.replace(/,/g,''));
+}
+
+const operations=["+","-","*","/"];
+var operator = document.getElementsByClassName("operator");
+for(var i =0;i<operator.length;i++){
+	operator[i].addEventListener('click',function(){
+  	console.log("i should run for every operation : ", numOps)
+		if(this.id=="clear"){
+			printHistory("");
+			printOutput("");
+      numOps = 0
+		}
+		else if(this.id=="backspace"){
+    	
+			var output=reverseNumberFormat(getOutput()).toString();
+			if(output){//if output has a value
+      	lastvalue = output[output.length - 1];
+  			if (operations.includes(lastvalue)){
+    				num -= 1;
+ 				}
+				output= output.substr(0,output.length-1);
+				printOutput(output);
+			}
+		}
+    else if(numOps >= 50 && this.id == "="){
+    	alert("Limit exceeded!!! Maximum 50 operands are allowed at a time");
+    }
+		else{
+    	
+			var output=getOutput();
+			var history=getHistory();
+			if(output==""&&history!=""){
+				if(isNaN(history[history.length-1])){
+					history= history.substr(0,history.length-1);
+				}
+			}
+			if(output!="" || history!=""){
+				output= output==""?output:reverseNumberFormat(output);
+				history=history+output;
+				if(this.id=="="){
+					var result=eval(history);
+					printOutput(result);
+					printHistory("");
+				}
+				else{
+        			if(operations.includes(this.id)) {  numOps+=1 }
+					history=history+this.id;
+					printHistory(history);
+					printOutput("");
+				}
+			}
+		}
+		
+	});
+}
+var number = document.getElementsByClassName("number");
+for(var i =0;i<number.length;i++){
+	number[i].addEventListener('click',function(){
+		console.log("number event i assume ")
+		var output=reverseNumberFormat(getOutput());
+		if(output!=NaN){ //if output is a number
+			output=output+this.id;
+			printOutput(output);
+		}
+	});
+} 
